597d33d9c80c0b66a640961e20c7cafcf7c28667
Gjorgji20 <goko.sirakoski@gmail.com>
2022-11-20 00:14:38 +0100

    exercises_basic

---
+++ JavaScript/Exercises/main.js
@@ -0,0 +1,15 @@
+console.log("Hello")
+
+let length=16;
+
+let lastName="Johnson";
+
+console.log(length);
+
+console.log(lastName);
+
+let a=10;
+let b=19;
+let c=a/b;
+console.log(c);
+console.log("the sum of "+a+"and "+b+" is"+c);
