f8f897234863f16530e470377ef5df184c431c84
Gjorgji20 <goko.sirakoski@gmail.com>
2022-11-22 20:48:07 +0100

    concatenation

---
+++ JavaScript/02_JavaScriptEssentials/Ex2.2/main.js
@@ -0,0 +1,6 @@
+let name="Gjorgji"
+let age=26
+let canCode=true;
+
+alert(`Hello, my name is ${name}, I am ${age} yeares old and i can code JavaScript ${canCode}`);
+console.log(`Hello, my name is ${name}, I am ${age} yeares old and i can code JavaScript ${canCode}`);
