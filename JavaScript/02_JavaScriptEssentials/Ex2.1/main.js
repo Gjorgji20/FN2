6ea986d027ff6494f53b3f407e42f11a23ac8df3
Gjorgji20 <goko.sirakoski@gmail.com>
2022-11-22 19:57:24 +0100

    type of variables

---
+++ JavaScript/02_JavaScriptEssentials/Ex2.1/main.js
@@ -0,0 +1,8 @@
+let str1='Laurence';
+let str2="Svekis";
+let val1=undefined;
+let cal2=null;
+let myNum=1000;
+
+alert("str1 "+typeof(str1)+"\nstr2 "+typeof(str2)+"\nval1 "+typeof(val1)+"\ncal2 "+typeof(cal2)+"\nmyNum "+typeof(myNum));
+console.log("str1 "+typeof(str1)+"\nstr2 "+typeof(str2)+"\nval1 "+typeof(val1)+"\ncal2 "+typeof(cal2)+"\nmyNum "+typeof(myNum));
