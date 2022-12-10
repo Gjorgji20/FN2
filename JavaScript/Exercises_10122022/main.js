e9e19cd0df75281d159b7442d60f2abe3557ed83
Gjorgji20 <goko.sirakoski@gmail.com>
2022-12-10 19:07:23 +0100

    if and switch condition

---
+++ JavaScript/Exercises_10122022/main.js
@@ -0,0 +1,146 @@
+// console.log("//1")
+// let no1=parseInt(prompt("No1"));
+// let no2=parseInt(prompt("No2"));
+
+// if(no1>no2)
+// {
+//     console.log(no1);
+// }else
+// {
+//     console.log(no2);
+// }
+// console.log("//2", no1)
+
+// if(no1%2==0)
+// {
+//     console.log("paren e");
+// }else
+// {
+//     console.log("neparen e");
+// }
+// console.log("//3")
+
+// let no3=parseInt(prompt("no3"));
+// if(!isNaN(no3))
+// {
+//     console.log("Broj e")
+// }else
+// {
+//     console.log("ne e broj");
+// }
+// console.log("//4")
+
+
+// let no4=parseInt(prompt("No1"));
+// let no5=parseInt(prompt("No2"));
+// if(no4==no5)
+// {
+//     console.log("are equal");
+// }else 
+// if(no4>no5)
+// {
+//     console.log(no4);
+// }else
+// {
+//     console.log(no5);
+// }
+// console.log("//5")
+
+// let no7=parseInt(prompt("No7"));
+// let no8=parseInt(prompt("No8"));
+// let no9=parseInt(prompt("No9"));
+// let max=0;
+// if(no7>no8)
+// {
+//     max=no7;
+// }else
+// {
+//     max=no8;
+// }
+// if(max<no9)
+// {
+//     max=no9
+// }
+//console.log(max);
+// console.log("//6")
+// let no7=parseInt(prompt("No7"));
+// let no8=parseInt(prompt("No8"));
+// let no9=parseInt(prompt("No9"));
+// if(no7==no8 && no7==no9)
+// {
+//     console.log("Equilateral triangle")
+// }else if(no7==no8 || no8==no9)
+// {
+//     console.log("Isosceles  triangle")
+// }else
+// {
+//     console.log("Scalene triangle.")
+//}
+//  console.log("//7")
+
+// let x1=parseInt(prompt("x1"));
+// let x2=parseInt(prompt("x2"));
+// let x=parseInt(prompt("x"));
+// if(x1<=x && x<=x2)
+// {
+//  console.log("Between the range")  
+// }else
+// {
+//     console.log("outside the range")
+// }
+// console.log("//8")
+
+// let operation=prompt("oper");
+// let x3=parseInt(prompt("x3"));
+// let x4=parseInt(prompt("x4"));
+// let res=0;
+// switch(operation)
+// {
+//     case "add":
+//         res=x3+x4;
+//         break;
+//         case "subtract":
+//         res=x3-x4;
+//         break;
+//         case "multiply":
+//         res=x3*x4;
+//         break;
+        
+//         case "divide":
+//         res=x3/x4;
+//         break;
+        
+//         case "modulus":
+//         res=x3%x4;
+//         break;
+//         default:
+//             console.log("Invalid opretation")
+// ;}
+//console.log(res)
+// console.log("//9")
+
+
+// let x5=parseInt(prompt("x5"));
+
+// if(x5>90 && x5<=100)
+// {
+//     console.log("S grade")
+// }else if(x5>80 && x5<=90)
+// {
+//     console.log("A grade")
+// }else if(x5>70 && x5<=80)
+// {
+//     console.log("B grade")
+// }else if(x5>60 && x5<=70)
+// {
+//     console.log("C grade")
+// }else if(x5>50 && x5<=60)
+// {
+//     console.log("D grade")
+// }else if(x5>40 && x5<=50)
+// {
+//     console.log("E grade")
+// }else
+// {
+//     console.log("Invalid marks");
+// }
