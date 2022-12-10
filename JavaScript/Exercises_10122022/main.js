34d1ded80a1ddfcec20c2b4e85178808f33d5583
Gjorgji20 <goko.sirakoski@gmail.com>
2022-12-10 19:45:48 +0100

    adittion examples

--- JavaScript/Exercises_10122022/main.js
+++ JavaScript/Exercises_10122022/main.js
@@ -144,3 +144,174 @@
 // {
 //     console.log("Invalid marks");
 // }
+
+// console.log("switch 1");
+// let no=parseInt(prompt("Day as number"));
+// switch (no)
+// {
+//     case 1:
+//         console.log("Mon")
+//         break;    
+//         case 2:
+//             console.log("Thu")
+//             break;
+            
+//     case 3:
+//         console.log("Wed")
+//         break;
+        
+//     case 4:
+//         console.log("Thr")
+//         break;
+        
+//     case 5:
+//         console.log("Fri")
+//         break;
+        
+//     case 6:
+//         console.log("Sat")
+//         break;
+        
+//     case 7:
+//         console.log("Sun")
+//         break;
+//     default:
+//         console.log("Invalid")
+// }
+
+// console.log("switch 2")
+// let godina=2022;
+// let no=parseInt(prompt("Day as number"));
+// switch (no)
+// {
+//     case 1:
+//         console.log("31")
+//         break;    
+//         case 2:
+//             if(godina%4==0 && godina%100!=0) || godina%400==0)
+//             {
+//                 console.log("29")
+//             }else
+//             {
+//                 console.log("28")
+//             }
+//             console.log("Thu")
+//             break;
+            
+//     case 3:
+//         console.log("31")
+//         break;
+        
+//     case 4:
+//         console.log("30")
+//         break;
+        
+//     case 5:
+//         console.log("31")
+//         break;
+        
+//     case 6:
+//         console.log("30")
+//         break;
+        
+//     case 7:
+//         console.log("31")
+//         break;
+
+//         case 8:
+//         console.log("31")
+//         break;
+//         case 9:
+//         console.log("30")
+//         break;
+//         case 10:
+//         console.log("31")
+//         break;
+
+//         case 11:
+//         console.log("30")
+//         break;
+//         case 12:
+//         console.log("31")
+//         break;
+//     default:
+//         console.log("Invalid")
+// }
+
+// console.log("switch 4")
+
+
+//  let n0 = 5;
+// switch (true) {
+//     case (n0 < 5):
+//         console.log("Number is very low");
+//         break;
+//     case (n0 < 10):
+//         console.log("Number is low");
+//         break;
+//     case (n0 > 100):
+//         console.log("Number is high");
+//         break;
+//     default:
+//         console.log("Number is medium")
+// }
+
+// console.log("switch 5")
+// let n0 = 100;
+// switch (n0) {
+//   case 0:
+//   case 5:
+//   case 6:
+//     console.log("Number is low");
+//     break;
+//   case 100:
+//     console.log("Number is high");
+//     break;
+//   default:
+//     console.log("I don't know the number");
+// }
+
+let a=Math.floor(Math.random()*11);
+
+switch(a)
+{
+    case 1:
+        console.log("Number is:"+a)
+        break;    
+        case 2:
+          
+            console.log("Number is:"+a)
+            break;
+            
+    case 3:
+        console.log("Number is:"+a)
+        break;
+        
+    case 4:
+        console.log("Number is:"+a)
+        break;
+        
+    case 5:
+        console.log("Number is:"+a)
+        break;
+        
+    case 6:
+        console.log("Number is:"+a)
+        break;
+        
+    case 7:
+        console.log("Number is:"+a)
+        break;
+
+        case 8:
+        console.log("Number is:"+a)
+        break;
+        case 9:
+        console.log("Number is:"+a)
+        break;
+        case 10:
+        console.log("Number is:"+a)
+        break;
+    default:
+        console.log("System error");
+}
